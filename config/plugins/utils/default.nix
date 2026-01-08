efd252802e7e3aa7cc156054cb44f901747028e4
AzumiA <hitrel@outlook.com>
2026-01-08 10:27:12 +0800

    add leetcode-nvim
    add leetcode-nvim

--- config/plugins/utils/default.nix
+++ config/plugins/utils/default.nix
@@ -11,8 +11,7 @@
     ./surround.nix
     ./img-clip.nix
     ./yazi.nix
-    # NOTE: 什么!!!居然有两个markdown的插件!!!
-    # NOTE: 不愧是我，贴心，且保持优雅!!!
+    ./leetcode.nix
     ./markdown-preview.nix # 一个能打开浏览器渲染看的
     ./render-markdown.nix # 一个ssh链接后，只能在终端显示的
   ];
---
+++ config/plugins/utils/leetcode.nix
@@ -0,0 +1,9 @@
+{pkgs, ...}: {
+  extraPlugins = with pkgs.vimPlugins; [
+    leetcode-nvim
+  ];
+
+  plugins.leetcode = {
+    enable = true;
+  };
+}
